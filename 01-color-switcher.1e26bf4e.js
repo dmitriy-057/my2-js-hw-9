f52196326dcb03266407abf368680ab56cddfdc3
dmitriy-057 <97353762+dmitriy-057@users.noreply.github.com>
2025-04-12 22:22:14 +0000

    Deploying to gh-pages from @ dmitriy-057/my2-js-hw-9@1e65858412dc1fc66d7c0ebe6b11a893891a349e 🚀

--- 01-color-switcher.1e26bf4e.js
+++
@@ -1,2 +0,0 @@
-const t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")};t.startBtn.addEventListener("click",(function(n){const e=setInterval((()=>{e&&(t.startBtn.disabled=!0),document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),t.stopBtn.addEventListener("click",(function(){clearInterval(null)}));
-//# sourceMappingURL=01-color-switcher.1e26bf4e.js.map
