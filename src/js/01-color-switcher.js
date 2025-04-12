20d4e5df75997d02e3bdc5f6e98284e301266529
Dmitriy Gonharov <my.webpage057@gmail.com>
2025-04-13 00:13:51 +0300

    update colorsitcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const refs = {
+    startBtn: document.querySelector("button[data-start]"),
+    stopBtn: document.querySelector("button[data-stop]"),
+};
+let intervalId = null;
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener("click", onStopBtnClick);
 
+function onStartBtnClick(evt) {
+    
+    const intervalId = setInterval(()=> {
+        if(intervalId) {
+            refs.startBtn.disabled = true;
+        }
+    document.body.style.backgroundColor = getRandomHexColor()
+    },1000);
+};
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  };
+
+  function onStopBtnClick() {
+    clearInterval(intervalId);
+  }
